b4364653da79cc1d7bf67eee8905a885b60c543c
yin19790111 <yin19790111@gmail.com>
2019-05-23 23:53:37 +0500

    Исправлена отрисовка добавляемой линии. Добавлено выделение вершин ломаной линии

--- VectorGraphicEditor/MainWindow.xaml.cs
+++ VectorGraphicEditor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -11,30 +13,32 @@ namespace VectorGraphicEditor
     public partial class MainWindow : Window
     {
         private Polyline _currentPolyline;
+        private Rectangle _marker;
         private Point _currentPoint;
         private bool _isAddNewObject;
+        private int? _pointIndex;
+        private List<int> _addedMarkerIndexes = new List<int>();
         public MainWindow()
         {
             InitializeComponent();
-            _isAddNewObject = false;            
+            _isAddNewObject = false;
         }
 
         private void btnAddLine_Click(object sender, RoutedEventArgs e)
         {
             _isAddNewObject = true;
+            addNewLin();
+        }
+
+        private void addNewLin()
+        {
             _currentPolyline = new Polyline
             {
                 Stroke = Brushes.Red
             };
+            _currentPolyline.MouseDown += Polyline_MouseDown;
+            _pointIndex = null;
             drawTable.Children.Add(_currentPolyline);
-        }
-
-        private void drawTable_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Right)
-            {
-                _isAddNewObject = false;
-            }
         }
 
         private void drawTable_MouseDown(object sender, MouseButtonEventArgs e)
@@ -45,12 +49,19 @@ namespace VectorGraphicEditor
                 {
                     _currentPoint = new Point(e.GetPosition(drawTable).X, e.GetPosition(drawTable).Y);
                     _currentPolyline.Points.Add(_currentPoint);
+                    if (_pointIndex == null) { _pointIndex = 0; }
+                    else { _pointIndex++; }
                     if (_currentPolyline.Points.Count == 1)
                     {
                         _currentPoint = new Point(e.GetPosition(drawTable).X, e.GetPosition(drawTable).Y);
                         _currentPolyline.Points.Add(_currentPoint);
+                        _pointIndex++;
                     }
                 }
+            }
+            if (e.ChangedButton == MouseButton.Right)
+            {
+                addNewLin();
             }
         }
 
@@ -58,13 +69,50 @@ namespace VectorGraphicEditor
         {
             if (_isAddNewObject && _currentPoint != null && _currentPolyline != null && _currentPolyline.Points.Count > 1)
             {
-                _currentPolyline.Points.Remove(_currentPoint);
+                _currentPolyline.Points.RemoveAt(_pointIndex.Value);
                 _currentPoint.X = e.GetPosition(drawTable).X;
                 _currentPoint.Y = e.GetPosition(drawTable).Y;
-                _currentPolyline.Points.Add(_currentPoint);
-                txtMessage.Text = "("+_currentPoint.X.ToString() + ";" + _currentPoint.Y.ToString() + ")";
+                _currentPolyline.Points.Insert(_pointIndex.Value, _currentPoint);
+                txtMessage.Text = "(" + _currentPoint.X.ToString() + ";" + _currentPoint.Y.ToString() + ")";
                 drawTable.InvalidateVisual();
             }
         }
+
+        private void BtnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            _isAddNewObject = false;
+        }
+
+        private void Polyline_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_isAddNewObject)
+            {
+                return;
+            }
+
+            if (_currentPolyline != (Polyline)sender && _addedMarkerIndexes.Count!= 0)
+            {
+                _addedMarkerIndexes.Reverse();
+                foreach (int idx in _addedMarkerIndexes)
+                {
+                    drawTable.Children.RemoveAt(idx);
+                }
+                _addedMarkerIndexes.Clear();
+            }
+            _currentPolyline = (Polyline)sender;
+            foreach(Point pnt in _currentPolyline.Points)
+            {
+                _marker = new Rectangle();
+                _marker.Stroke = Brushes.Green;
+                _marker.Fill = Brushes.Transparent;
+                _marker.Width = 10;
+                _marker.Height = 10;
+                Canvas.SetLeft(_marker, pnt.X - 5);
+                Canvas.SetTop(_marker, pnt.Y - 5);
+                _addedMarkerIndexes.Add(drawTable.Children.Add(_marker));
+            }
+        }
+
+
     }
 }
